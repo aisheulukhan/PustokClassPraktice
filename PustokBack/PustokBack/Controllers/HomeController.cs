d7f7bf899e39c089d1345fd5bd33d0ee6a88b156
Aysha Ulukhanova <tu7hnsxs1@aztu.local>
2022-05-23 16:45:02 +0400

    add

---
+++ PustokBack/PustokBack/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PustokBack.DAL;
+using PustokBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PustokBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            List<Slider> sliders = _context.Sliders.ToList();
+            return View(sliders);
+        }
+    }
+}
